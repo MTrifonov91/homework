5c72722fbb40835f3447ed0bfb87f0c6ff834038
Mihail Trifonov <trifonovmihail@mail.ru>
2018-08-07 23:01:23 +0300

    Solved the task in a different way

---
+++ Task2.27 (Checkerboard Pattern of Asterisks)/Stars3.java
@@ -0,0 +1,23 @@
+public class Stars3{
+
+	public static void main (String args[]){
+
+	for (int i = 1; i<=8; i++){
+			if (i%2==0) {
+				System.out.println ("");
+				System.out.print (" ");
+				}
+			else {
+				System.out.println ("");
+			}
+		for (int a = 1; a<=16; a++){
+			if (a%2==0) {
+				System.out.print (" ");
+				}
+			else {
+				System.out.print ("*");
+			}
+					}
+				}	
+			}	
+		}
