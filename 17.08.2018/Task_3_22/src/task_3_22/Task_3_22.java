c3c70189b300c16ebd5a7f9da086291e9d185636
User <trifonovmihail@mail.ru>
2018-08-22 18:31:02 +0300

    Solved Task_3_22

---
+++ 17.08.2018/Task_3_22/src/task_3_22/Task_3_22.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task_3_22;
+
+/**
+ *
+ * @author User
+ */
+public class Task_3_22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        for (int i=1;i<=5;i++){
+            //for (int j=1; j<=4; j++){
+                System.out.println (i + "   " + (i*10) + "   " + (i*100) + "   " + (i*1000));
+                /*switch (j){
+                    case 1: System.out.print(i + "     "); break;
+                    case 2: System.out.print((i*10) + "     "); break;
+                    case 3: System.out.print((i*100) + "     "); break;
+                    case 4: System.out.println((i*1000) + "     "); break;
+               */
+                }
+                
+            }
+        }
+        // TODO code application logic here
+  //  }
+    
+
