fe826aa46d56bcce6d101327f4d5f66e543ab151
User <trifonovmihail@mail.ru>
2018-08-22 18:44:57 +0300

    Solved Task_3_33

---
+++ 17.08.2018/Task_3_33/src/task_3_33/Task_3_33.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task_3_33;
+
+/**
+ *
+ * @author User
+ */
+public class Task_3_33 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=2;
+        for (;;){
+            System.out.println(a);
+            a=a*2;
+            
+    }
+        // TODO code application logic here
+    }
+    
+}
