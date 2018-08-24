4cf7b0571baeb5bd73db6f5245a80398011b0089
Mihail Trifonov <trifonovmihail@mail.ru>
2018-08-24 08:18:55 +0300

    Half way through Task_6 Perfect numbers

--- 22.08.2018/Task_6/src/task_6/Task_6.java
+++ 22.08.2018/Task_6/src/task_6/Task_6.java
@@ -16,9 +16,23 @@ public class Task_6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        for (int i =1; i<=1000; i++){
-            if ()
-        }
-    }
-    
+        int devider;
+        int perfect;
+        int sum_of_deviders=0;
+        for (int i =2; i<=10; i++){
+            System.out.println("i is " + i);
+            for (int j = 1; j<i; j++){
+                System.out.println("j is " + j);
+                     if (j<i){ 
+                    if (i%j==0){
+                    devider = j;
+                        System.out.println("devider is " + devider);
+                        
+                        sum_of_deviders += devider;
+                        System.out.println("sum is " + sum_of_deviders);
+                    }              
+    }  
+}
+}
+}
 }
