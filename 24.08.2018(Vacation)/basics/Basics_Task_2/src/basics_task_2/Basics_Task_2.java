6c8c4e9b4a8ea2064de8f000ea3eead91fcfca52
Mihail Trifonov <trifonovmihail@mail.ru>
2018-08-28 22:32:22 +0300

    Solved Basics Task_2

--- 24.08.2018(Vacation)/basics/Basics_Task_2/src/basics_task_2/Basics_Task_2.java
+++ 24.08.2018(Vacation)/basics/Basics_Task_2/src/basics_task_2/Basics_Task_2.java
@@ -16,6 +16,13 @@ public class Basics_Task_2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String quote = "I fucking hate pikeys";
+        String movieName = "Snatch";
+        String characterName = "Tommmy";
+        int yearOfTheMovie = 2001;
+        System.out.println("My favourite movie quote is: " + quote + "\n" + "Comes from the movie: " + movieName);
+        System.out.println("It was said by charcater: " + characterName);
+        System.out.println("The movie was released in: " + yearOfTheMovie);
     }
     
 }
