d0b298e950032fcf915f4ec28da4cb228f63659a
Mihail Trifonov <trifonovmihail@mail.ru>
2018-08-28 23:13:39 +0300

    Solved Basics Task_3

---
+++ 24.08.2018(Vacation)/basics/Basics_Task_3/src/basics_task_3/Basics_Task_3.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basics_task_3;
+
+/**
+ *
+ * @author User
+ */
+public class Basics_Task_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.printf("X" + "%24s", "X" + "\n");
+        System.out.printf("X" + "%24s", "X" + "\n");
+        System.out.printf("X" + "%16s", "XXXXXXXXXX");
+        System.out.printf("%8s", "X" + "\n");
+        System.out.printf("XXXXX" + "%3s", "X");
+        System.out.printf("%9s", "X");
+        System.out.printf("%8s", "XXXXX" + "\n");
+        System.out.printf("X" + "%4s", "X");
+        System.out.printf("%3s", "X");
+        System.out.printf("%9s", "X");
+        System.out.printf("%3s", "X");
+        System.out.printf("%5s", "X" + "\n");
+        System.out.printf("X" + "%4s", "X");
+        System.out.printf("%3s", "X");
+        System.out.printf("%9s", "X");
+        System.out.printf("%3s", "X");
+        System.out.printf("%7s", "X" + "\n\n\n"); 
+        System.out.println("X                      X");
+        System.out.println("X                      X");
+        System.out.println("X      XXXXXXXXXX      X");
+        System.out.println("XXXXX  X        X  XXXXX");
+        System.out.println("X   X  X        X  X   X");
+        System.out.println("X   X  X        X  X   X");
+        
+        
+        
+      
+    }
+    
+}
