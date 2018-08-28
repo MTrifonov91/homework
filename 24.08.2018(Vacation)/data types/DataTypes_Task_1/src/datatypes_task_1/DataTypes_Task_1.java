13dad4dec8f14132a97a465bc2e73e8599a9a50e
Mihail Trifonov <trifonovmihail@mail.ru>
2018-08-28 23:32:37 +0300

    Solved Data types Task_1

---
+++ 24.08.2018(Vacation)/data types/DataTypes_Task_1/src/datatypes_task_1/DataTypes_Task_1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datatypes_task_1;
+
+/**
+ *
+ * @author User
+ */
+public class DataTypes_Task_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char firstName = 'M';
+        char lastName = 'T';
+        char fatherName = 'I';
+        System.out.println(firstName + "." + lastName + "." + fatherName);
+    }
+    
+}
