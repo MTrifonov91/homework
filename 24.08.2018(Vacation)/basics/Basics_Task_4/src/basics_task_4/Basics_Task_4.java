f8d1df9d1587b5419512b97430f992a45208ccd7
Mihail Trifonov <trifonovmihail@mail.ru>
2018-08-28 23:18:49 +0300

    Solved Basics Task_4

---
+++ 24.08.2018(Vacation)/basics/Basics_Task_4/src/basics_task_4/Basics_Task_4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basics_task_4;
+
+/**
+ *
+ * @author User
+ */
+public class Basics_Task_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String motto = "Carly's makes the food that makes it a party";
+        System.out.println(motto);
+                
+    }
+    
+}
