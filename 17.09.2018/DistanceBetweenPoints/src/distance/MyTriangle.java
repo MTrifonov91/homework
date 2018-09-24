f6ba94c418883583bb33b968256b32c129c1a207
Mihail Trifonov <trifonovmihail@mail.ru>
2018-09-25 00:10:43 +0300

    Task my Triangle from Distance between package is solved correctly

--- 17.09.2018/DistanceBetweenPoints/src/distance/MyTriangle.java
+++ 17.09.2018/DistanceBetweenPoints/src/distance/MyTriangle.java
@@ -15,7 +15,12 @@ public class MyTriangle {
     private MyPoint v2;
     private MyPoint v3;
    
-    
+    public MyTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+        this.v1 = new MyPoint(x1, y1);
+        this.v2 = new MyPoint(x2, y2);
+        this.v3 = new MyPoint(x3, y3);
+        
+    }
    
     
     public MyTriangle(MyPoint v1, MyPoint v2, MyPoint v3) {
@@ -26,9 +31,28 @@ public class MyTriangle {
 
     @Override
     public String toString() {
-        return "MyTriangle{" + v1 + "=(" + getX() + "," + getY() + ")," +
-                v2 + "=(" + x2 + "," + y2 + ")," +
-                v3 + "=(" + x3 + "," + x3 + "),"
+        return "MyTriangle[v1=(" + v1 + "), v2={" + v2 + "}, v3=(" + v3 + ")";
     }
+    
+    public double getPerimeter() {
+        
+        return this.v1.distance(v2) + this.v2.distance(v3) + this.v3.distance(v1);
+    }
+    
+    public String getType() {
+        if (this.v1.distance(v2)==this.v2.distance(v3)&&
+                this.v1.distance(v2)==this.v3.distance(v1)){
+            return "This triangle is Equilateral";
+        }
+        if (this.v1.distance(v2)==this.v2.distance(v3)||
+                this.v1.distance(v2)==this.v3.distance(v1)||
+                    this.v2.distance(v3)==this.v3.distance(v1)){
+            return "This triangle is Isosceles";
+        }
+        else{
+            return "This triangle is Scalene";
+        }
+    }
+    
     
 }
--- 17.09.2018/DistanceBetweenPoints/src/distance/Test.java
+++ 17.09.2018/DistanceBetweenPoints/src/distance/Test.java
@@ -25,6 +25,20 @@ public class Test {
         System.out.println(newPosition.distance(6, 8));
         System.out.println(newPosition.distance(anotherPosition));
         System.out.println(newPosition.distance());
+        
+        System.out.println("_____________________________");
+        MyTriangle firstTriangle = new MyTriangle(2,3,4,5,9,6);
+        System.out.println(firstTriangle);
+        System.out.println(firstTriangle.getPerimeter());
+        System.out.println(firstTriangle.getType());
+        
+        MyTriangle secondTriangle = new MyTriangle(9,9,14,11,9,13);
+        System.out.println(secondTriangle.getPerimeter());
+        System.out.println(secondTriangle.getType());
+        
+        MyTriangle thirdTriangle = new MyTriangle(16,3,20,6,16,9);
+        System.out.println(thirdTriangle.getPerimeter());
+        System.out.println(thirdTriangle.getType());
     }
     
 }
