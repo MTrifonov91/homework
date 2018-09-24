869cfbe75370dd566be1f28ab8de5293d4caf569
User <trifonovmihail@mail.ru>
2018-09-24 22:23:28 +0300

    Added my Triangle class

--- 17.09.2018/DistanceBetweenPoints/src/distance/MyTriangle.java
+++ 17.09.2018/DistanceBetweenPoints/src/distance/MyTriangle.java
@@ -11,4 +11,24 @@ package distance;
  */
 public class MyTriangle {
     
+    private MyPoint v1;
+    private MyPoint v2;
+    private MyPoint v3;
+   
+    
+   
+    
+    public MyTriangle(MyPoint v1, MyPoint v2, MyPoint v3) {
+        this.v1 = new MyPoint();
+        this.v2 = new MyPoint();
+        this.v3 = new MyPoint();
+    }
+
+    @Override
+    public String toString() {
+        return "MyTriangle{" + v1 + "=(" + getX() + "," + getY() + ")," +
+                v2 + "=(" + x2 + "," + y2 + ")," +
+                v3 + "=(" + x3 + "," + x3 + "),"
+    }
+    
 }
